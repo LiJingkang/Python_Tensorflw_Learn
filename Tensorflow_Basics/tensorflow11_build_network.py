c91f6b5ad8f595326ad2649a00f2213b8a6c4431
LiJingkang <LiJingkang@outlook.com>
2017-12-12 21:35:48 +0800

    20171212

--- Tensorflow_Basics/tensorflow11_build_network.py
+++ Tensorflow_Basics/tensorflow11_build_network.py
@@ -17,31 +17,49 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
         outputs = activation_function(Wx_plus_b)
     return outputs
 
+##############################################################
 # Make up some real data
+# 创造真实数据
 x_data = np.linspace(-1,1,300)[:, np.newaxis]
+# 加入噪声  初始值0  方差0.05 格式 x_data.shape
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
+##############################################################
 # define placeholder for inputs to network
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
+
+##############################################################
 # add hidden layer
+# 定义隐藏层
+# 使用 placeholder 传进来的值
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 # add output layer
+# prediction 预计数据
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
+##############################################################
 # the error between prediciton and real data
+# loss 函数，差别。  reduce_sum 求和  tf.square 平方（每个例子的平方）
+# 对每一个例子求和，然后对他求一个平均值  rf.reduce_mean
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
                      reduction_indices=[1]))
+# 在误差里面如何学习
+# Gradient 梯度   Descent 下降  Optimizer 优化程序
+# .minimize（loss） 对误差进行更正
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
+
+##############################################################
 # important step
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+init = tf.initialize_all_variables()  # 初始化
+sess = tf.Session() # 定义 Session
+sess.run(init) # 设置 session 的指针。进行计算
 
 for i in range(1000):
     # training
+    #
     sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
         # to see the step improvement
